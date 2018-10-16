1dff4372599f2dd80da76ea71722c004eb9e1ba6
André "MG" Wisén <andre.wisen@gmail.com>
2018-10-16 21:49:56 +0200

    Fix config.ini

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,1 @@
-#!/bin/sh
-# Run this to set up the build system: configure, makefiles, etc.
-set -e
-
-srcdir=`dirname $0`
-test -n "$srcdir" && cd "$srcdir"
-
-echo "Updating build configuration files for USDX, please wait..."
-
-autoreconf -isf
+[H[2J
